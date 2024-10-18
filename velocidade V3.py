e5f7cd06d2f26d55d5849435fb2c611442c56a10
Raul Sergio Ferreira <raulstar3@gmail.com>
2024-10-18 13:01:57 -0300

    v0.1

--- velocidade V3.py
+++ velocidade V3.py
@@ -152,6 +152,7 @@ def main():
         cv2.imshow("Detecção de Velocidade", frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
+            
             break
 
     vs.release()
